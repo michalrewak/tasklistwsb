bd79bac33fffb65e10628359f7ebe0b56b036b2b
michalrewak <michalrewak@gmail.com>
2022-04-10 18:53:53 +0200

    merge

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm 
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy  import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'blabla'
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config["SECRET_KEY"] = "blabla"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./test.db"
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,26 +30,41 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me')
+    username = StringField(
+        "username", validators=[InputRequired(), Length(min=4, max=15)]
+    )
+    password = PasswordField(
+        "password", validators=[InputRequired(), Length(min=8, max=80)]
+    )
+    remember = BooleanField("remember me")
+
 
 class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    email = StringField(
+        "email",
+        validators=[InputRequired(), Email(message="Invalid email"), Length(max=50)],
+    )
+    username = StringField(
+        "username", validators=[InputRequired(), Length(min=4, max=15)]
+    )
+    password = PasswordField(
+        "password", validators=[InputRequired(), Length(min=8, max=80)]
+    )
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
 
@@ -50,42 +73,47 @@ def login():
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
-                return redirect(url_for('dashboard'))
+                return redirect(url_for("dashboard"))
 
-        return '<h1>Invalid username or password</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        return "<h1>Invalid username or password</h1>"
+        # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
-    return render_template('login.html', form=form)
+    return render_template("login.html", form=form)
 
-@app.route('/signup', methods=['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = generate_password_hash(form.password.data, method="sha256")
+        new_user = User(
+            username=form.username.data, email=form.email.data, password=hashed_password
+        )
         db.session.add(new_user)
         db.session.commit()
 
-        return '<h1>New user has been created!</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        return "<h1>New user has been created!</h1>"
+        # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
-    return render_template('signup.html', form=form)
+    return render_template("signup.html", form=form)
 
-@app.route('/new_task')
-def hello_world():  # put application's code here
-    return render_template('create_new_task.html')
+
+@app.route("/new_task")
+def new_task():  # put application's code here
+    return render_template("create_new_task.html")
 
 
 @app.route("/add_new_task", methods=["GET", "POST"])
 def change_url():
     new_task = {}
-    new_task['title'] = request.form["title"]
-    new_task['description'] = request.form["description"]
-    new_task['priority'] = request.form["priority"]
-    new_task['assignee'] = request.form["assignee"]
-    new_task['status'] = request.form["status"]
-    print(new_task)
+    new_task["title"] = request.form["title"]
+    new_task["description"] = request.form["description"]
+    new_task["priority"] = request.form["priority"]
+    new_task["assignee"] = request.form["assignee"]
+    new_task["status"] = request.form["status"]
+    return render_template("create_new_task.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=False)
