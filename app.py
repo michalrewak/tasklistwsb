f47da982332ec513d01b1bdde94afeb0f6e4f2a2
msdfib <enricopl@hotmail.it>
2022-04-16 14:46:39 +0200

    del-task

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from enum import Enum, auto
+
+from flask import Flask, render_template, redirect, request, session
+import flask_login
+from flask_session import Session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import (
     LoginManager,
     UserMixin,
-    login_user,
-    login_required,
-    logout_user,
-    current_user,
+    login_user, logout_user,
 )
 import dbmanager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "blabla"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./test.db"
+SESSION_TYPE = "filesystem"
+app.config.from_object(__name__)
+Session(app)
 bootstrap = Bootstrap(app)
 test = dbmanager
 login_manager = LoginManager()
@@ -25,10 +27,16 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 
+class Priority(Enum):
+    high = auto()
+    medium = auto()
+    low = auto()
+
+
 class User(UserMixin):
     def __init__(self, id, email):
-        self.id = id
-        self.email = email
+        self.id = int(id)
+        self.email = str(email)
 
 
 @login_manager.user_loader
@@ -37,12 +45,8 @@ def load_user(user_id):
 
 
 class LoginForm(FlaskForm):
-    email = StringField(
-        "email", validators=[InputRequired(), Email(message="Invalid email"), Length(max=50)]
-    )
-    password = PasswordField(
-        "password", validators=[InputRequired(), Length(min=8, max=80)]
-    )
+    email = StringField("email", validators=[InputRequired(), Email(message="Invalid email"), Length(max=50)])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField("remember me")
 
 
@@ -51,14 +55,18 @@ class RegisterForm(FlaskForm):
         "email",
         validators=[InputRequired(), Email(message="Invalid email"), Length(max=50)],
     )
-    password = PasswordField(
-        "password", validators=[InputRequired(), Length(min=8, max=80)]
-    )
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8, max=80)])
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    logout_user()
+    return redirect('/login')
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -69,20 +77,13 @@ def login():
         userId = test.loginUser(form.email.data, form.password.data)
         if userId != -1:
             session["_user_id"] = userId
-            user = User(id, test.userEmail(id))
+            user = User(userId, test.userEmail(id))
             login_user(user, remember=form.remember.data)
-            return redirect(url_for("index"))
+            return redirect("tasks")
 
         return "<h1>Invalid email or password</h1>"
-        # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
     return render_template("login.html", form=form)
-
-
-@app.route("/logout", methods=["GET", "POST"])
-def logout():
-    logout_user()
-    return redirect('/login')
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -92,32 +93,50 @@ def signup():
     if form.validate_on_submit():
         test.createUser(form.email.data, form.password.data)
         return "<h1>New user has been created!</h1>"
-        # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
     return render_template("signup.html", form=form)
 
 
 @app.route("/add_new_task", methods=["GET", "POST"])
-def new_task():  # put application's code here
-    # new_task = {}
-    # new_task["title"] = request.form["title"]
-    # new_task["description"] = request.form["description"]
-    # new_task["priority"] = request.form["priority"]
-    # new_task["assignee"] = request.form["assignee"]
-    # new_task["status"] = request.form["status"]
-    # db.session.add(new_task)
-    # db.session.commit()
+def new_task():
     return render_template("create_new_task.html")
+
+
+@app.route("/new_task_commit", methods=["GET", "POST"])
+def new_task_commit():
+    userid = flask_login.current_user._get_current_object().id
+    test.insertTask(
+        request.form["title"],
+        Priority[request.form["priority"]].value,
+        userid,
+        request.form["description"],
+        request.form["status"],
+    )
+    return redirect("tasks")
 
 
 @app.route("/tasks", methods=["GET"])
 def tasks_list():
-    if '_user_id' not in session:
+    if "_user_id" not in session:
         return redirect("signup")
 
-    user_id = session['_user_id']
-    tasks = test.getTasks(user_id)
+    userid = flask_login.current_user._get_current_object().id
+    tasks = test.getTasks(userid)
 
+    return render_template("tasks.html", items=tasks)
+
+
+@app.route("/delete_task/<task_id>,<assignee>", methods=["POST"])
+def delete_task(task_id, assignee):
+    if "_user_id" not in session:
+        return redirect("signup")
+    user_id = flask_login.current_user._get_current_object().id
+    user_email = test.userEmail(user_id)
+
+    if assignee != user_email:
+        return str(user_email)
+    test.delete_task(task_id)
+    tasks = test.getTasks(user_id)
     return render_template("tasks.html", items=tasks)
 
 
